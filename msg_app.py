419ac45075a9a6783839f8e36524eab6e06d7712
Daniil Z <zubkov_dn@mail.ru>
2018-10-08 13:24:28 +0300

    Написаны методы для управления gui: вывод полученного текста в лог, включение и выключение кнопок. Написаны сигналы для старта и завершения фонового потока

--- msg_app.py
+++ msg_app.py
@@ -13,6 +13,8 @@ class VkMessApp(QtWidgets.QWidget, gui.Ui_VkMessenger):
         self.btn_start.clicked.connect(self.start_scan)
         self.btn_stop.clicked.connect(self.stop_scan)
         self.btn_login.clicked.connect(self.login)
+        self.thread.started.connect(self.on_started)
+        self.thread.finished.connect(self.on_finished)
         self.thread = msg_scan.MsgScan() # Поиск в отдельном потоке
         self.thread.result_signal.connect(self.show_result)
         self.thread.success_signal.connect(self.success_alarm)
@@ -26,11 +28,23 @@ class VkMessApp(QtWidgets.QWidget, gui.Ui_VkMessenger):
 
     def show_result(self, result):
         '''Добавляет запись в лог и проматывает его вниз'''
-        pass
+        self.log.textCursor().insertText(result)
+        self.log.ensureCursorVisible()
+
+    def on_started(self):
+        ''' Вынесено из start_scan, чтобы кнопка start блокировалась только
+        когда фоновый поток успешно запущен'''
+        self.btn_start.setDisabled(True)
+        self.btn_stop.setDisabled(False)
+
+    def on_finished(self):
+        self.btn_start.setDisabled(False)
+        self.btn_stop.setDisabled(True)
 
     def success_alarm(self):
         '''Окно разворачивается, всплывает'''
-        pass
+        self.showNormal()
+        self.activateWindow()
 
     def login(self):
         ''' Вызов авторизации'''
