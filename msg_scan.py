a71a80ccf084c5c141498fe96bff967944fcbb2f
annaconda <annaconda@bk.ru>
2018-10-10 22:31:58 +0300

    Заменила метод на правильный, сделала проверки

--- msg_scan.py
+++ msg_scan.py
@@ -47,8 +47,37 @@ class MsgScan(QThread):
 ##        return messages
 
     def get_name(self, id, token):
-        ''' Вернуть имя и фамилию'''
+        ''' Вернуть имя и фамилию,
+        (может работать как с ключом доступа пользователя,
+        так и с сервисным ключом доступа)'''
         session = vk.Session(access_token=token)
         api = vk.API(session, v='5.85')
-        name = api.account.getProfileInfo.first_name + api.account.getProfileInfo.last_name
+        user = api.users.get(user_id=id)
+        name = user[0]['first_name'] + ' ' + user[0]['last_name']
         return name
+
+#сервисный ключ доступа (постоянный)
+token1 = 'b4c325c5b4c325c5b4c325c5f4b4a5a586bb4c3b4c325c5ef7d6e8321dd82c069e344a5'
+#ключ доступа пользователя (временный)
+token2 = 'a6a1493a15fca6c13035e79bd10bf039bde3526983a217cfb909b4364888a97b0451b68c01130369f2a2f'
+my_id = 2318509
+
+msg_scan = MsgScan()
+print(msg_scan.get_name(my_id, token1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
