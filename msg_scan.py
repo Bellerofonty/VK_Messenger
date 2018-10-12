8175d79179aca5acc19ed96b0c3bc13f901f60b5
Brutalfacepalm <theshick@mail.ru>
2018-10-12 19:15:02 +0400

    get_history возвращает словарь: id сообщения : [отправитель, текст сообщения]

--- msg_scan.py
+++ msg_scan.py
@@ -18,11 +18,6 @@ class MsgScan(QThread):
     def run(self):
         ''' Вызывается при запуске потока.
         Вызывать остальные методы отсюда.
-        # self.get_conversations(self.read_token())
-        self.get_history(id, unread_count, self.read_token())
-        # id - пользователя с кем выдать историю
-        # unread_count получить откуда то или задать вручную
-
         Вывод результата в лог:
             self.result_signal.emit(output)
         Сигнал о новом сообщении (окно разворачивается и всплывает):
@@ -30,8 +25,6 @@ class MsgScan(QThread):
 
     def read_token(self):
         ''' Прочитать из файла и вернуть токен для запросов'''
-        return 'token'
-        # тут может быть ваш токен
 
 ##        return token
 
@@ -44,16 +37,6 @@ class MsgScan(QThread):
         "unread_count"
         для каждого диалога
         '''
-        session = vk.Session(access_token=token)
-        api = vk.API(session, v='5.85')
-        # Получаем непрочитанные диалоги
-        response_dialogs = api.messages.getConversations(filter='unread')
-        for count in range(response_dialogs.get('count')):
-            # print(response_dialogs)
-            unread_count = ((response_dialogs.get('items')[count]).get('conversation')).get('unread_count')
-            id = (((response_dialogs.get('items')[count]).get('conversation')).get('peer')).get('id')
-            print(id)
-            print(unread_count)
 
 ##        return unread_conv_list
 
@@ -62,18 +45,8 @@ class MsgScan(QThread):
         session = vk.Session(access_token=token)
         api = vk.API(session, v='5.85')
         messages_history = api.messages.getHistory(count = unread_count, user_id = id)['items'][::-1]
-        my_name = self.get_name(None, token)
-        id_name = self.get_name(id, token)
-        for messages in messages_history:
-            print((id_name if messages['from_id'] == id else my_name) + ': ' + messages['text'])
+        history = {messages['id']:[messages['from_id'], messages['text']] for messages in messages_history}
+        return history
 
     def get_name(self, id, token):
         ''' Вернуть имя и фамилию'''
-        session = vk.Session(access_token=token)
-        api = vk.API(session, v='5.85')
-        name = api.users.get(user_id = id)
-        return name[0]['first_name'] + ' ' + name[0]['last_name']
-
-
-msg_scan = MsgScan()
-msg_scan.run()
