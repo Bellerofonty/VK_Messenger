54b20fff34b96abb4c4c0aa2bf02d17395fc7ebd
Daniil Z <zubkov_dn@mail.ru>
2018-10-20 14:58:58 +0300

    Revert "Merge pull request #8 from Bellerofonty/msg_scan_run"
    This reverts commit fabdfbeff277f4137202e2c192b1408eed590871, reversing
    changes made to 700d944f3324dd871545346b02e589fdc5af9224.

--- msg_app.py
+++ msg_app.py
@@ -10,41 +10,23 @@ class VkMessApp(QtWidgets.QWidget, gui.Ui_VkMessenger):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.btn_start.clicked.connect(self.start_scan)
-        self.btn_stop.clicked.connect(self.stop_scan)
-        self.btn_login.clicked.connect(self.login)
         self.thread = msg_scan.MsgScan() # Поиск в отдельном потоке
-        self.thread.started.connect(self.on_started)
-        self.thread.finished.connect(self.on_finished)
         self.thread.result_signal.connect(self.show_result)
         self.thread.success_signal.connect(self.success_alarm)
 
     def start_scan(self):
-        self.thread.start()
+        pass
 
     def stop_scan(self):
-        self.thread.terminate()
-        self.on_finished()
+        pass
 
     def show_result(self, result):
         '''Добавляет запись в лог и проматывает его вниз'''
-        self.log.textCursor().insertText(result)
-        self.log.ensureCursorVisible()
-
-    def on_started(self):
-        ''' Вынесено из start_scan, чтобы кнопка start блокировалась только
-        когда фоновый поток успешно запущен'''
-        self.btn_start.setDisabled(True)
-        self.btn_stop.setDisabled(False)
-
-    def on_finished(self):
-        self.btn_start.setDisabled(False)
-        self.btn_stop.setDisabled(True)
+        pass
 
     def success_alarm(self):
         '''Окно разворачивается, всплывает'''
-        self.showNormal()
-        self.activateWindow()
+        pass
 
     def login(self):
         ''' Вызов авторизации'''
