64db196724a8d571d4e4d54819da17cd936ff625
Ivan_Ko <kovalenko_ivan@hotmail.com>
2018-11-05 08:37:05 +0300

    Add login thread methods.

--- msg_app.py
+++ msg_app.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QInputDialog as dialog
+from PyQt5.QtWidgets import QLineEdit as input_field
+from PyQt5.QtWidgets import QMessageBox as message_box
 
 import gui
 import msg_scan
 import login
+
 
 class VkMessApp(QtWidgets.QWidget, gui.Ui_VkMessenger):
     '''GUI и управление потоком запросов'''
@@ -19,7 +23,6 @@ class VkMessApp(QtWidgets.QWidget, gui.Ui_VkMessenger):
         self.thread.result_signal.connect(self.show_result)
         self.thread.success_signal.connect(self.success_alarm)
         self.btn_login.clicked.connect(self.login)
-        self.login_thread = login.Login_Thread()
 
     def start_scan(self):
         self.thread.start()
@@ -49,5 +52,47 @@ class VkMessApp(QtWidgets.QWidget, gui.Ui_VkMessenger):
         self.activateWindow()
 
     def login(self):
-        ''' Вызов авторизации'''
+        '''Getting token.
+
+        Ask user to authenticate.
+        Deliver credentials to login thread.
+        Send requset with credentials to API to get token.
+        Write token to a file.
+        Answer wirh result message.'''
+
+        credentials = self.authenticate_dialog()
+
+        self.login_thread = login.Login_Thread(credentials[0],
+                                               credentials[1])
+
+        self.login_thread.notification_string.connect(
+            self.notification_window)
+        '''Transmit notification message from login thread
+        to main thread(GUI).'''
+
         self.login_thread.start()
+        '''Request to API and write token to the file.'''
+
+    def authenticate_dialog(self):
+        '''Input login and password.'''
+
+        _login, ok = dialog.getText(
+            None,
+            'VKMessenger Authorization',
+            'Type login',
+            input_field.Normal
+        )
+
+        _password, ok = dialog.getText(
+            None,
+            'VKMessenger Authorization',
+            'Type password',
+            input_field.Password
+        )
+
+        return _login, _password
+
+    def notification_window(self, text_arg):
+        '''Notify about success/errors in login thread.'''
+        self.display_msg = message_box()
+        self.display_msg.about(self, 'Notification', text_arg)
