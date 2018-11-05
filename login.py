9dc24cdfb13299786fcf15d3dfdb8d60e7333243
Ivan_Ko <kovalenko_ivan@hotmail.com>
2018-11-05 08:34:53 +0300

    Refactoring. Remove GUI piece. Clean.

--- login.py
+++ login.py
@@ -1,2 +1,58 @@
-﻿''' Авторизация ВК (в браузере или в окне приложения),
-        получение токена, сохранение его в файл token.txt'''
+import vk
+from vk.exceptions import VkAuthError
+
+from PyQt5.QtCore import \
+    QThread, \
+    pyqtSignal
+
+
+CLIENT_ID = '6713283'  # Applications ID
+
+
+class Login_Thread(QThread):
+
+    notification_string = pyqtSignal(str)
+
+    def __init__(self, _login, _password, parent=None):
+        super(QThread, self).__init__()
+        self._login = _login
+        self._password = _password
+
+    def run(self):
+        '''Main method of thread.'''
+        try:
+            token = self._request_token(self._login,
+                                        self._password)
+
+            result_string = self._write_token_to_file(token)
+
+            self.notification_string.emit(result_string)
+
+        except VkAuthError:
+            self.notification_string.emit(
+                'Not valid login or password\n'
+                'Неверный логин или пароль')
+
+        self.exit()  # or could use sys.exit()
+        '''Exit Thread.'''
+
+    def _request_token(self, login, password):
+        '''Start auth session, send request to API.'''
+
+        self.session = vk.AuthSession(
+            app_id=CLIENT_ID,
+            user_login=login,
+            user_password=password,
+            scope='4096',
+        )
+
+        return self.session.get_access_token()
+
+    def _write_token_to_file(self, token):
+
+        with open('token.txt', 'w+') as f:
+            f.write(token)
+
+        return \
+            'Token has been written\n'\
+            'Токен записан в файл'
